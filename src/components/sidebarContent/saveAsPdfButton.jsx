5296c84134a052934c8026184fb377faf934714a
David Steiner <david1.steiner@gmail.com>
2024-06-10 17:50:23 +0200

    updated save as pdf button functionality

--- src/components/sidebarContent/saveAsPdfButton.jsx
+++ src/components/sidebarContent/saveAsPdfButton.jsx
@@ -1,12 +1,12 @@
-import savePageAsPDF from '../../scripts/savePageAsPDF';
-
 export default function SavePageAsPDFButton({}) {
 
   function handleClick() {
-    if (confirm('This functionality is not working correctly right now, are you sure you want to save the page as a PDF?')) savePageAsPDF()
+    if (!confirm('This functionality is broken right now, are you sure?')) return
+
+    window.print();
   }
 
   return(
-    <button className='save-button' onClick={handleClick}>Save page as PDF</button>
+    <button className='save-button' onClick={handleClick}>Save as PDF</button>
   )
 }
